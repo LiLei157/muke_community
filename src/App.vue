3f8e9a4ce38b4988271a85c7ff4bc844174e34f6
ll <ll@gonsin.cn>
2020-10-24 17:40:52 +0800

    慕课社区项目初始化

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <common-header></common-header>
+  </div>
+</template>
+
+<script>
+import CommonHeader from '@/components/CommonHeader'
+export default {
+  name: 'App',
+  data(){
+    return{
+      
+    }
+  },
+  components: {
+    // HelloWorld
+    CommonHeader
+  },
+  mounted(){
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
